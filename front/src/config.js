2aaebc9406e45c633ce70f191688cce2c644da13
Vicente Barberá Tormo <barberatormov@gmail.com>
2025-04-17 15:20:56 +0200

    Update project structure for Vercel monorepo deployment

--- front/src/config.js
+++ front/src/config.js
@@ -4,9 +4,7 @@ const isDevelopment =
   window.location.hostname === '127.0.0.1'
 
 const config = {
-  apiURL: isDevelopment
-    ? 'http://localhost:3000/api/v1'
-    : 'https://proyecto10-rm-backend.vercel.app/api/v1' // URL de tu backend en Vercel
+  apiURL: isDevelopment ? 'http://localhost:3000/api/v1' : '/api/v1' // Ahora usamos una ruta relativa ya que todo está en el mismo dominio
 }
 
 export default config
